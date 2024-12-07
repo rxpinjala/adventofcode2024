ab88ecb1ef395492fdb702f0081da8a3fa44fca6
Ravi Pinjala <ravixpinjala@gmail.com>
2024-12-06 22:23:58 -0800

    Day 1 completed

--- day1.js
+++ day1.js
@@ -1,3 +1,4 @@
+// @ts-check
 
 function parse_numbers(input) {
     let lines = input.trim().split("\n");
@@ -29,18 +30,89 @@ function compare_numbers_in_order(lists) {
     return result;
 }
 
-export function day1() {
+/**
+ * Calculate a total similarity score by adding up each number in the left list
+ * after multiplying it by the number of times that number appears in the right list.
+ */
+function compute_similarity_score(lists) {
+    let list1 = lists.list1;
+    let list2 = lists.list2;
 
+    // We can compute the similarity score in O(n log n) time by sorting
+    // the lists and finding consecutive runs of the same number
+    list1.sort();
+    list2.sort();
+
+    let run_length = function(list, i) {
+        let count = 1;
+        while (i < (list.length - 1) && list[i] == list[i + 1]) {
+            count++;
+            i++;
+        }
+        return count;
+    }
+
+    let i1 = 0;
+    let i2 = 0;
+    let result = 0;
+
+    while (i1 < list1.length && i2 < list2.length) {
+        // Get the next value from list1, and compute the run length in list1
+        let current_num = list1[i1];
+        let rl1 = run_length(list1, i1);
+
+        // Search for a matching value in list2
+        while (i2 < list2.length && list2[i2] < current_num) {
+            i2++;
+        }
+        if (i2 == list2.length) {
+            break;
+        }
+        if (list2[i2] > current_num) {
+            // No matches for this run in list1, move to the next one
+            i1 += rl1;
+            continue;
+        }
+
+        // Sanity check
+        if (list2[i2] != current_num) {
+            throw new Error("This shouldn't happen");
+        }
+
+        // Compute the run length in list2
+        let rl2 = run_length(list2, i2);
+        let similarity = current_num * rl1 * rl2;
+        result += similarity;
+
+        // Advance to the next run
+        i1 += rl1;
+        i2 += rl2;
+    }
+
+    return result;
 }
 
-export function test1() {
-    const testData = `3   4
+const provided_test_input = 
+`3   4
 4   3
 2   5
 1   3
 3   9
 3   3`;
-    let lists = parse_numbers(testData);
+
+/**
+ * 
+ * @param {string} input
+ * @param {HTMLElement} outputDiv 
+ */
+function run_day1a(input, outputDiv) {
+    const lists = parse_numbers(input);
+    const result = compare_numbers_in_order(lists);
+    outputDiv.innerText = String(result);
+}
+
+function test_day1a() {
+    let lists = parse_numbers(provided_test_input);
     console.assert(_.isEqual(lists.list1, [3, 4, 2, 1, 3, 3]));
     console.assert(_.isEqual(lists.list2, [4, 3, 5, 3, 9, 3]));
 
@@ -48,3 +120,29 @@ export function test1() {
     console.log(result);
     console.assert(result === 11);
 }
+
+function run_day1b(input, outputDiv) {
+    const lists = parse_numbers(input);
+    const result = compute_similarity_score(lists);
+    outputDiv.innerText = String(result);
+}
+
+function test_day1b() {
+    const lists = parse_numbers(provided_test_input);
+    const result = compute_similarity_score(lists);
+    console.assert(result === 31);
+}
+
+export const day1a = {
+    name: "Day 1a",
+    default_input: provided_test_input,
+    run_func: run_day1a,
+    test_func: test_day1a,
+};
+
+export const day1b = {
+    name: "Day 1b",
+    default_input: provided_test_input,
+    run_func: run_day1b,
+    test_func: test_day1b,
+};
--- main.js
+++ main.js
@@ -1,17 +1,96 @@
-import { day1, test1 } from "./day1.js";
+// @ts-check
+import { day1a, day1b } from "./day1.js";
 
-const appDivElement = document.getElementById("app");
-
-const navItems = [
-    { name: "Day 1a", func: () => day1() }
-];
-
-const navUlElement = document.getElementById("nav-list");
-for (const item of navItems) {
-    let itemElement = document.createElement('li');
-    itemElement.textContent = item.name;
-    itemElement.addEventListener('click', item.func);
-    navUlElement.appendChild(itemElement);
+/**
+ * @param {string} id 
+ * @returns {HTMLElement}
+ */
+function requireElementById(id) {
+    const element = document.getElementById(id);
+    if (element === null) {
+        throw new Error("Can't find #" + id);
+    }
+    return element;
 }
 
-test1();
+const navSelectorForm = requireElementById("nav-list");
+const inputDiv = requireElementById('input');
+const inputText = requireElementById('input-text');
+const outputDiv = requireElementById('output');
+const runButton = requireElementById('run-button');
+
+
+/**
+ * @typedef {Object} NavItem
+ * @property {string} name
+ * @property {string} default_input
+ * @property {function()} run_func
+ * @property {function()} test_func
+ * 
+ * @type {NavItem[]}
+ */
+const navItems = {
+    "day1a": day1a,
+    "day1b": day1b,
+};
+
+function getSelectedNavItem() {
+    let selectedItem = document.querySelector('input[name="day"]:checked');
+    if (selectedItem === null || !(selectedItem instanceof HTMLInputElement)) {
+        return "";
+    }
+    return selectedItem.value;
+}
+
+/**
+ * @param {HTMLElement} form
+ * @param {string} value 
+ * @param {string} display 
+ */
+function addNavRadioButton(form, value, display) {
+    // Create a container element
+    let pElement = document.createElement('p');
+    pElement.setAttribute('class', 'nav-item');
+
+    // Create <input> element
+    let inputElement = document.createElement('input');
+    inputElement.setAttribute('type', 'radio');
+    inputElement.setAttribute('name', 'day');
+    inputElement.setAttribute('value', value);
+    inputElement.setAttribute('id', value);
+
+    // Create <label> element
+    let labelElement = document.createElement('label');
+    labelElement.setAttribute('for', value);
+    labelElement.innerText = display;
+
+    // Wire everything together
+    pElement.appendChild(inputElement);
+    pElement.appendChild(labelElement);
+    form.appendChild(pElement);
+}
+
+// Set up left nav list
+for (const navItem in navItems) {
+    addNavRadioButton(navSelectorForm, navItem, navItems[navItem].name);
+}
+
+// Set up event handlers
+navSelectorForm.addEventListener('change', function (event) {
+    if (event.target == null || !(event.target instanceof HTMLInputElement)) {
+        return;
+    }
+    if (event.target.type == 'radio' && event.target.name == 'day') {
+        const navItem = navItems[event.target.value];
+        inputText.value = navItem.default_input;
+    }
+});
+
+runButton.addEventListener('click', function (event) {
+    const selectedItem = getSelectedNavItem();
+    if (!(selectedItem in navItems)) {
+        return;
+    }
+    const navItem = navItems[selectedItem];
+    navItem.run_func(inputText.value, outputDiv);
+});
